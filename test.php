0974d9deaac529f9dd43312ef427b6dd980a304e
nukefrog <nico5469@gmail.comm>
2017-04-18 18:15:28 +0200

    finalisation chapter 1

--- test.php
+++ test.php
@@ -1,3 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Ceci est une page de test avec du PHP </title>
+  <meta charset="utf-8" />
+  <meta name="description" content="Un chiffre aléatoire va être choisi entre 1 et 10 grâce au code PHP qui suit : <?php echo rand(1, 10); ?>" />
+</head>
+<body>
+  <h2> Page de Test </h2>
+  <p>
+    Cette Page Contient du code HTML et du PHP <br />
+    <?php echo "Test PHP"; ?>
+    Voici Quelques petits Tests :
+  </p>
+  <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y H:i:s'); echo "semaine ";echo date(W); ?>.</p>
+  <p><?php echo "Cette ligne a été écrite \"uniquement\" en PHP."; ?></p>
+  
 
-echo '<h1>Bonjour à tous</h1>';
+  <ul>
+    <li style="color: blue;"> Texte en Bleu </li>
+    <li style="color: red;"> Texte en Rouge </li>
+    <li style="color: green;"> Texte en vert </li>
+  </ul>
+
+
+</body>
